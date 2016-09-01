5b6b3a25e1b81552d72e07ca2e7e8b63b52fc699
adityavchaturvedi <aditya2013@berkeley.edu>
2016-09-01 13:01:00 -0700

    First try

---
+++ main_scraper.py
@@ -0,0 +1,17 @@
+""" Importing appropriate packages"""
+import requests # Package to get raw data from websites.
+import gzip # CONSIDER DELETING THIS
+import csv 
+import json
+import urllib # Package to support requests
+import re # Regular Expressions
+import time
+#import psycopg2
+#from psycopg2.extensions import AsIs
+import sys
+from bs4 import BeautifulSoup # For cleaning up HTML raw data.
+#from Queue import Queue
+#from threading import Thread
+#from multiprocessing import Process, Lock
+
+
